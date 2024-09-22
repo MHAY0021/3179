ba894a65a7e2545102a0620f349469bd284b2137
MHAY0021 <161259211+MHAY0021@users.noreply.github.com>
2024-09-22 22:06:51 +1000

    Add files via upload

---
+++ map.js
@@ -0,0 +1,7 @@
+var vg_2 = "vax.json";
+
+vegaEmbed("#map", vg_2).then(function(result) {
+
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
